c9dc1fe8a83a296f178b087374697a12119b149a
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-07-24 20:31:30 +0600

    removed item from cart

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { setBook } from "../redux/cart/reducer";
+import { useSelector } from "react-redux";
+import { removeBook, setBook } from "../redux/cart/reducer";
 
-export default function Button({ book }) {
+export default function Button({ book, onClick }) {
+  const items = useSelector((state) => state.cart.booksInCart);
+  const isBookInCart = items.some((item) => item.id === book.id);
   const dispatch = useDispatch();
-  const handleClick = () => {
-    dispatch(setBook(book));
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (isBookInCart) {
+      dispatch(removeBook(book.id));
+    } else {
+      dispatch(setBook(book));
+    }
   };
 
   return (
     <div className="button">
       <button onClick={handleClick} className="add_button">
-        В корзину
+        {isBookInCart ? "Убрать из корзины" : "В корзину"}
       </button>
     </div>
   );
