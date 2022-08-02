529a3f7e964c633d71b6bed9c9be5191f7a37e60
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-08-02 23:50:18 +0600

    made a transition to the cart page through the Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { useCallback } from "react";
 import { useNavigate } from "react-router";
+import { NavLink } from "react-router-dom";
 export default function Header() {
-  const navigate = useNavigate();
-  const handleClick = useCallback(() => {
-    navigate("/cart");
-  }, [navigate]);
   return (
     // <header>
     <div className="header">
       <div className="container">
         <div className="header_elements">
           <div className="header_title">BookShop</div>
-          <div className="header_cart_icon" onClick={handleClick}>
-            <i className="fa-solid fa-cart-shopping" />
-          </div>
+          <NavLink to="/cart">
+            <div className="header_cart_icon">
+              <i className="fa-solid fa-cart-shopping" />
+            </div>
+          </NavLink>
         </div>
       </div>
     </div>
